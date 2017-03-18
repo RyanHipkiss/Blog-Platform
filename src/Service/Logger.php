69e1776f1717c3c136a311b5cb6b1ec40d7d2843
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2017-03-18 16:16:33 +0000

    Log to file, retrieve all errors

--- src/Service/Logger.php
+++ src/Service/Logger.php
@@ -4,9 +4,15 @@ namespace App\SErvice;
 
 class Logger 
 {
-
+	const FILE = __DIR__ . '/../../logs/error.log';
 	public static function send($error)
 	{
-		die($error);
+		$error = date('[jS M Y - H:i:s]') . ' ' . $error;
+		error_log($error . PHP_EOL, 3, self::FILE);
+	}
+
+	public static function all()
+	{
+		return file_get_contents(self::FILE);
 	}
 }
