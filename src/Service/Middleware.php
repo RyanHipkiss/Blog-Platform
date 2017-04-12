d6829a5bf337b299c9399b5de0f60ea391ae35be
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2017-04-12 21:08:23 +0100

    Authenticate on home route

---
+++ src/Service/Middleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use App\Service\Session;
+use App\Service\Redirect;
+use App\Service\Router;
+
+class Middleware
+{
+    public function loggedIn(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        if(empty(Session::user())) {
+            Redirect::to(Router::getUriFromName('login'));
+        }
+        
+        return $next($request, $response);
+    }
+}
